7b52da2cbeafdf62a3d0d38a1aa4917ba2c05fd2
Bryan Vera <brayanandresveragar@gmail.com>
2024-03-27 12:40:56 -0500

    :construction: Creación de la pagina contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,12 @@
+import { Form } from "../../components/Form/Form";
 import "./Contact.css";
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <p>I am always open to discussing new projects, opportunities in tech world,partnerships and more so mentorship. </p>
+      <Form></Form>
+    </div>
   )
 }
 
